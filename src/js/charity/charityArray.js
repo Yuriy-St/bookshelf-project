c726ebd45e1f0404daefe0fb99b76a281c8e4beb
mellesacha <0251817@gmail.com>
2023-07-09 18:37:51 +0400

    markup2

--- src/js/charity/charityArray.js
+++ src/js/charity/charityArray.js
@@ -3,47 +3,47 @@ const charityArray = [
     title: 'Save the Children',
     url:
       'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
+    img: "../../img/charity/save-children.png",
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
+    img: "../../img/charity/hope.png",
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
+    img: "../../img/charity/united.png",
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
+    img: "../../img/charity/med-corps.png",
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
+    img: "../../img/charity/med-sans.png",
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
+    img: "../../img/charity/razom.png",
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
+    img: "../../img/charity/action-against.png",
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
+    img: "../../img/charity/world-vision.png",
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
+    img: "../../img/charity/prytula.png",
   },
 ]
 
--- src/js/charity/charityScrollContainer.js
+++ src/js/charity/charityScrollContainer.js
@@ -1,23 +1,34 @@
-import charityArray from "./charityArray.js"
+import charityArray from "./charityArray.js";
+
+const charityBlock = document.querySelector('.charity');
+
+charityBlock.innerHTML =
+    `<div class="charity-header"><h2 class="charity-title">Support Ukraine</h2>
+    <img class="charity-logo" src="../../svg/ua.svg" alt=""></div>
+    <ul class="charity-list"></ul>
+    <button class="btn-scroll" type="button">
+    <img class="arrow-down" src="../../svg/down-arrow.svg" alt="" width="20" height="20">
+    </button>`;
 
 const charityList = document.querySelector('.charity-list');
 const btnScroll = document.querySelector('.btn-scroll');
+const imgArrow = document.querySelector('.arrow-down')
 
 function renderCharityList() {
-    return charityArray.map((e, i) => `<li class="charity-item"><p>${addLeadingZero(i+1)}</p><a href=${e.url}>${e.title}</a></li>` )
+    return charityArray.map((e, i) => `<li class="charity-item">
+    <p class="counter">${addLeadingZero(i + 1)}</p>
+    <a href=${e.url} target="_blank"><img src=${e.img} alt="${e.title}" height="32px" class="foundation-logo"/></a></li>`)
 };
 
 function addLeadingZero(value) {
     return value.toString().padStart(2, '0')
-}
+};
 
 export default function markupCharityList() {
     charityList.innerHTML = renderCharityList().join('')
-}
+};
 
 const options = {
-//   root: charityList,
-//   rootMargin: "0px",
   threshold: 1.0, 
 };
 
@@ -35,20 +46,25 @@ btnScroll.addEventListener('click', onClick)
 
 function onClick() {
 
-    charityList.scrollBy({
+    observer.observe(charityList.lastElementChild);
+
+    if (!charityList.lastElementChild.isIntersecting) {
+        charityList.scrollBy({
             top: 100,
             behavior: "smooth",
         })
+    }
 
-    observer.observe(charityList.lastElementChild);
+    if (imgArrow.classList.contains("arrow-up")) {
+        charityList.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })
+        imgArrow.classList.remove("arrow-up")
+    }
 }
 
 
 function onScrollTop() {
-    
-    charityList.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-    
+    imgArrow.classList.add("arrow-up")
 }
