27176bd6cf968befcdeb6cfd7547b60e55be0467
Nikolay Shein <qseboy1998@gmail.com>
2023-07-09 20:19:04 +0300

    add db API, change firebase config

--- src/js/firebase/firebase.js
+++ src/js/firebase/firebase.js
@@ -1,5 +1,6 @@
 // initial firebase
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAA8jYNKkxR3HcK5Tf_qvCbW3uqAVzZXEY',
@@ -9,6 +10,9 @@ const firebaseConfig = {
   messagingSenderId: '302463622298',
   appId: '1:302463622298:web:d325247a433ddd90885323',
   measurementId: 'G-TR194SD0XD',
+  databaseURL:
+    'https://bookshelf-24937-default-rtdb.europe-west1.firebasedatabase.app/',
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
