676615038f773c2624cf8741c6aca7aaddfcc2d0
Nikolay Shein <qseboy1998@gmail.com>
2023-07-09 13:35:35 +0300

    add authFirebaseApi, singup, singin. Add singup markup, styles

---
+++ src/js/firebase/firebase.js
@@ -0,0 +1,14 @@
+// initial firebase
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAA8jYNKkxR3HcK5Tf_qvCbW3uqAVzZXEY',
+  authDomain: 'bookshelf-24937.firebaseapp.com',
+  projectId: 'bookshelf-24937',
+  storageBucket: 'bookshelf-24937.appspot.com',
+  messagingSenderId: '302463622298',
+  appId: '1:302463622298:web:d325247a433ddd90885323',
+  measurementId: 'G-TR194SD0XD',
+};
+
+const app = initializeApp(firebaseConfig);
