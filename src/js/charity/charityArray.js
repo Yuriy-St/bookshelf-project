fa163137c85dc637c92cdd63ed54b5d3efae7b3a
Yuriy-St <yuriystaynov@gmail.com>
2023-07-10 12:41:57 +0300

    move charity imagies folder

--- src/js/charity/charityArray.js
+++ src/js/charity/charityArray.js
@@ -1,50 +1,49 @@
 const charityArray = [
   {
     title: 'Save the Children',
-    url:
-      'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: "../../img/charity/save-children.png",
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: '../../public/charity/save-children.png',
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: "../../img/charity/hope.png",
+    img: '../../public/charity/hope.png',
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: "../../img/charity/united.png",
+    img: '../../public/charity/united.png',
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: "../../img/charity/med-corps.png",
+    img: '../../public/charity/med-corps.png',
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: "../../img/charity/med-sans.png",
+    img: '../../public/charity/med-sans.png',
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: "../../img/charity/razom.png",
+    img: '../../public/charity/razom.png',
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: "../../img/charity/action-against.png",
+    img: '../../public/charity/action-against.png',
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: "../../img/charity/world-vision.png",
+    img: '../../public/charity/world-vision.png',
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: "../../img/charity/prytula.png",
+    img: '../../public/charity/prytula.png',
   },
-]
+];
 
 export default charityArray;
