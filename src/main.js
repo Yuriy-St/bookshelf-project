4c227357f0e001570e54c4c1d6cb622e8179139d
Yuriy Staynov <45993587+Yuriy-St@users.noreply.github.com>
2023-07-13 16:40:57 +0300

    Revert "Feature/#12 pop up modal"

--- src/main.js
+++ src/main.js
@@ -11,6 +11,3 @@ renderHeader();
 renderCategoriesScrollbar(categoriesContainerRef);
 renderBestSellerBooks(topBooksRef);
 renderCharityList();
-
-import renderBookModal from './js/shoppingList/modalBook'; //TODO remove, for testing only
-renderBookModal('643282b1e85766588626a0b4'); //TODO remove, for testing only
