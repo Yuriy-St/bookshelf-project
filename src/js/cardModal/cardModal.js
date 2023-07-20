6d686672db63aab52bf688eec6bb59b6a0db5ff6
VovaMyb <rimidalvatopayl@gmail.com>
2023-07-20 20:24:16 +0300

    Update cardModal.js

--- src/js/cardModal/cardModal.js
+++ src/js/cardModal/cardModal.js
@@ -58,7 +58,7 @@ const reRenderButtons = async id => {
 bookShelfEl.addEventListener('click', async event => {
   const targetElement = event.target.closest('li');
   const isQuickView = event.target.closest('.quick-view');
-
+  //
   if (isQuickView && targetElement.classList.contains('book_card')) {
     const book = await fetchBookById(targetElement.dataset.bookid);
     cardBackdrop.innerHTML = markupCardModal(book);
