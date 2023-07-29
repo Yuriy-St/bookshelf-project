d2d0734407546d7873fa6dbe00206d697dab636f
VovaMyb <rimidalvatopayl@gmail.com>
2023-07-29 11:11:47 +0300

    start fixing header and menu

--- src/js/checkSession.js
+++ src/js/checkSession.js
@@ -1,24 +1,46 @@
 import { getUserSession } from './storage/saveUser';
 
-const menuListEl = document.querySelector('.menu-list');
-const loginEl = document.querySelector('#sign-up-header-button');
-const loginWrapperEl = document.querySelector('#login-wrapper');
-const loginNameEl = document.querySelector('.login-name');
+const menuListEls = document.querySelectorAll('.menu-list');
+const loginEls = document.querySelectorAll('#sign-up-header-button');
+const loginWrapperEls = document.querySelectorAll('#login-wrapper');
+const loginNameEls = document.querySelectorAll('.login-name');
 
 export function checkSession() {
   const session = getUserSession();
 
   if (session) {
-    loginNameEl.textContent = session.user.displayName;
+    [...loginNameEls].map(
+      loginNameEl => (loginNameEl.textContent = session.user.displayName)
+    );
 
-    menuListEl.classList.remove('hidden');
-    loginWrapperEl.classList.remove('hidden');
-    loginEl.classList.add('hidden');
+    showUserMenu();
   } else {
-    menuListEl.classList.add('hidden');
-    loginWrapperEl.classList.add('hidden');
-    loginEl.classList.remove('hidden');
+    showLogin();
   }
+}
+
+function showUserMenu() {
+  toggleEls(menuListEls, show);
+  toggleEls(loginWrapperEls, show);
+  toggleEls(loginEls, hide);
+}
+
+function showLogin() {
+  toggleEls(menuListEls, hide);
+  toggleEls(loginWrapperEls, hide);
+  toggleEls(loginEls, show);
+}
+
+function hide(el) {
+  el.classList.add('hidden');
+}
+
+function show(el) {
+  el.classList.remove('hidden');
+}
+
+function toggleEls(elements, method) {
+  [...elements].map(el => method(el));
 }
 
 checkSession();
